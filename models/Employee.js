5433b40409f9af4268a9f206ee18add932ede8d9
medsaiefbardi <bardimohamedsaief@gmail.com>
2025-01-11 11:47:57 +0100

    oo5

--- models/Employee.js
+++ models/Employee.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const employeeSkillSchema = new mongoose.Schema({
   skill: { type: mongoose.Schema.Types.ObjectId, ref: 'Skill', required: true },
-  level: { type: String, enum: ['N/A', 'N', 'A', 'M', 'E'], default: 'N/A' }
+  level: { type: String, enum: ['N/A','N', 'A', 'M', 'E'], default: 'N/A' }
 });
 
 const employeeSchema = new mongoose.Schema({
@@ -10,12 +10,28 @@ const employeeSchema = new mongoose.Schema({
   password: { type: String, required: true },
   role: { type: String, enum: ['employee', 'hr_head'], required: true },
   jobPosition: { type: mongoose.Schema.Types.ObjectId, ref: 'JobPosition' },
-  skills: [employeeSkillSchema],
-  
-  // Store evolution metrics for each KPI and competence
-  evolutionKpi: { type: [Number], default: [0] }, // Array to hold evolution for each KPI
-  evolutionCompetence: { type: [Number], default: [0] } // Array for competence evolution
+  evaluation: {
+    objectivesPerformance: [
+      {
+        objective: { type: String },
+        indicator: { type: String },
+        weight: { type: Number },
+      },
+    ],
+    objectivesCompetence: [
+      {
+        objective: { type: String },
+        indicator: { type: String },
+        weight: { type: Number },
+      },
+    ],
+    totalPerformance: { type: Number, default: 0 },
+    totalCompetence: { type: Number, default: 0 },
+    totalEvaluation: { type: Number, default: 0 },
+  },
+  skills: [employeeSkillSchema]
 });
 
 const Employee = mongoose.model('Employee', employeeSchema);
+
 module.exports = Employee;
